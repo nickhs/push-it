21887c1628280c24729698c9040da3072cf95b65
Matt Carrier <mcarrier@astonish.com>
2014-02-27 11:08:52 -0500

    Fix symlink.

--- install.sh
+++ install.sh
@@ -2,6 +2,6 @@
 
 sudo mkdir /etc/push-it/
 sudo cp ./clip.wav /etc/push-it/clip.wav
-sudo ln -s push-it.sh /usr/bin/push-it.sh
+sudo ln -s $(pwd)/push-it.sh /usr/bin/push-it.sh
 sudo chmod a+x /usr/bin/push-it.sh
 echo "Done!"
